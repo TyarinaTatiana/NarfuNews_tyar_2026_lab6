4b638101e4cbeb70f3a0d9f9e2d557a5646b415b
tatyanatyarina <tyarina.tatyana@gmail.com>
2026-04-02 16:08:15 +0300

    add json data

--- Frontend/src/components/UI/NavBar.vue
+++ Frontend/src/components/UI/NavBar.vue
@@ -4,21 +4,28 @@
       <v-app-bar-nav-icon @click.stop="emit('toggle-drawer')"/>
     </template>
 
-    <v-app-bar-title><v-icon icon="$logoIcon"/>Новости</v-app-bar-title>
+    <v-app-bar-title style="margin-left: 4px"><v-icon style="margin-right: 12px" icon="$logoIcon"/>Новости</v-app-bar-title>
     <div style="width:35%;">
-      <v-text-field variant="outlined" hide-details density="compact"/>
+      <v-text-field 
+          class="nav-bar__search-field"
+          variant="outlined" 
+          hide-details density="compact"
+          placeholder="Поиск документа"
+          prepend-inner-icon="mdi-magnify"
+          
+      />
     </div>
     <template #append>
-      <v-btn v-if="!isAuth">
-        Войти
+      <v-btn v-if="!isAuth && !currentUser">
+        Войти 
       </v-btn>
      <v-menu v-else>
        <template v-slot:activator="{ on, attrs }">
          <v-btn
-             color="primary"
+             color="white"
              v-bind="props"
          >
-           ФИО
+           {{ currentUser }}
          </v-btn>
        </template>
      </v-menu>
@@ -27,16 +34,30 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import NarfuNewsData from '@/../NarfuNewsData.json'
 
 const emit = defineEmits(['toggle-drawer'])
 const isAuth = ref(false)
+
+const currentUser=computed(()=> {
+const currentUserData = NarfuNewsData.currentUser;
+const userName =currentUserData.secondName+' '+currentUserData.name[0]+'.';
+  return !!currentUserData.lastName&& currentUserData.lastName?.length > 0
+      ? userName+currentUserData.lastName[0]+'.'
+      :userName;
+}, ()=>{})
 
 
 
 </script>
 
 
-<style scoped>
+<style >
+.nav-bar__search-field{
+  .v-field__overlay{
+    background-color: rgba(256, 256, 256, 30%);
+  }
+}
 
 </style>
