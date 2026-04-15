fa3bf5dd7912a3979bd62a14484a4aa4418abcee
TyarinaTatiana <tyarina.tatyana@gmail.com>
2026-04-15 23:33:33 +0300

    LoginController

--- Server/controllers/LoginController.js
+++ Server/controllers/LoginController.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const tableService = require('../services/TableService');
 tableService.tableId = process.env.LOGIN_TABLE_ID;
 const express = require('express');
-const axios = require("axios");
 /*
  * работа с роутом (уникальным путем контроллера)
  * LoginController: Авторизация/регистрация
@@ -12,19 +11,19 @@ const router = express.Router();
 
 /*
     description: Маршрут для поиска пользователя по логину и паролю
-    router: http://localhost:3010/api/login/auth?login={login}&password={password}
+    router: http://localhost:3010/api/login/auth?email={email}&password={password}
     type: get
 */
 router.get('/auth', (req, res) => {
-    const { login, password } = req.query;
+    const { email, password } = req.query;
 
     // Проверка наличия обязательных полей
-    if (!login || !password) {
+    if (!email || !password) {
         //Возврат ошибки, если хотя бы одно из полей не заполнено
         return res.status(400).json({ message: 'Не заполнены обязательные поля' });
     }
     
-    tableService._get(`?filterByFormula=AND({login}='${login}', {password}='${password}')`)
+    tableService._get(`?filterByFormula=AND({email}='${email}', {password}='${password}')`)
         .then(req=> req.data)
         .then(records=> {
             if(records.total == 0){
@@ -35,3 +34,4 @@ router.get('/auth', (req, res) => {
 });
 
 module.exports = router;
+
